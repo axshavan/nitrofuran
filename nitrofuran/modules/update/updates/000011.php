ce963581570902424d83c7a707c5e25a9965252f
Axshavan <axshavan@yandex.ru>
2011-03-07 11:50:03 +0300

    Забыл добавить файл

---
+++ nitrofuran/modules/update/updates/000011.php
@@ -0,0 +1,11 @@
+<?php
+
+// 000011
+// добавление валют в планирование в кассе
+
+global $DB;
+require_once(DOCUMENT_ROOT.'/nitrofuran/modules/kassa/config.php');
+$DB->Query("alter table `".KASSA_PLANS_TABLE."` add column `currency_id` integer unsigned not null");
+$DB->Query("update `".KASSA_PLANS_TABLE."` set `currency_id` = 1");
+
+?>
